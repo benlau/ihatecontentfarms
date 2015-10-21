5ea8b5b9ca240c3aaf0bb316289d17c7692309b3
Ben Lau <xbenlau@gmail.com>
2015-10-21 21:26:34 +0800

    Filter: Support wildcard on site list.

--- src/cfblocker/Filter.js
+++ src/cfblocker/Filter.js
@@ -10,13 +10,14 @@ export default class Filter {
     appendBlackList(list) {
         for (var i in list) {
             var item = list[i];
-            var pattern = new RegExp("^[a-zA-Z0-9\.]*\\.+" + item+"$|^"+item +"$");
+            var replaced = item.replace(/\./g,"\\.").replace(/\*/g,".*");            
+            var pattern = new RegExp("^[a-z0-9\.]*\\.+" + replaced + "$|^" + replaced + "$");
             this._blackList.push(item);
             this._patterns.push(pattern);
         }
     }
     
-    filter(hostname) {
+    match(hostname) {
         for (var i in this._patterns) {
             var pattern = this._patterns[i];
             if (hostname.toLowerCase().match(pattern)) {
--- tests/filter_tests.js
+++ tests/filter_tests.js
@@ -4,17 +4,34 @@ import Filter from "cfblocker/Filter";
     
 describe("Filter", () => {
     
-    it("filter", () => {
+    it("match", () => {
         var filter = new Filter();
         filter.appendBlackList(["contentfarm.com"]);
         
-        assert(filter.filter("contentfarm.com"));
-        assert(filter.filter("www.contentfarm.com"));
-        assert(filter.filter("www2.en.contentfarm.com"));
-        assert(!filter.filter("xcontentfarm.com"));
-        assert(!filter.filter("facebook.com"));
-        assert(!filter.filter("contentfarm2.com"));
+        assert(filter.match("contentfarm.com"));
+        assert(filter.match("www.contentfarm.com"));
+        assert(filter.match("www2.en.contentfarm.com"));
+        assert(!filter.match("xcontentfarm.com"));
+        assert(!filter.match("facebook.com"));
+        assert(!filter.match("contentfarm2.com"));
         
     });
+    
+    it("match with asterisk", () => {
+        var filter = new Filter();
+        filter.appendBlackList(["contentfarm.blogspot.*","contentfarm.com"]);
+        
+        assert(filter.match("contentfarm.com"));
+        assert(filter.match("www.contentfarm.com"));
+        assert(filter.match("www2.en.contentfarm.com"));
+        assert(!filter.match("xcontentfarm.com"));
+        assert(!filter.match("facebook.com"));
+        assert(!filter.match("contentfarm2.com"));
+        
+        assert(filter.match("contentfarm.blogspot.com"));
+        assert(filter.match("contentfarm.blogspot.hk"));
+        assert(!filter.match("contentfarm.blogspotsite.com"));
+    });
+    
     
 });
