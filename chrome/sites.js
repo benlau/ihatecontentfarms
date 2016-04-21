7d10aedc17ff86bf8c198bec6693b0df0fe8ea15
Zack <zackexplosion@users.noreply.github.com>
2016-04-21 22:15:31 +0800

    Update sites.js

--- chrome/sites.js
+++ chrome/sites.js
@@ -98,6 +98,7 @@ var sites = [
     "superfun-e.com",
     "teepr.com",
     "thefundaily.com",
+    "thenewslens.com",
     "ttshow.tw",
     "tw.anyelse.com",
     "tw.jdkartsports.nl/", // Whole HK
